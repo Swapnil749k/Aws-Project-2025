d1aef9135a21c1d31f3e9f817dc08e767175c067
swapnilk749 <swapnilkale411@gmail.com>
2025-06-21 09:57:57 +0530

    project upload 1 stage

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cvsuso8s4xnw.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678',
+    DB_DATABASE : 'webappdb'
+});
